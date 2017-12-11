e0c1b70bebe2c05bd6bc9c47b0f77dad8a3380ec
Titaniumstein <evan333es@gmail.com>
2017-12-11 11:05:24 -0500

    added router navigation

---
+++ MacroManager.Controllers/Navigation/IRouter.cs
@@ -0,0 +1,14 @@
+﻿using MacroManager.Controllers.Navigation.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroManager.Controllers.Navigation
+{
+    public interface IRouter
+    {
+        void GoTo<TRoute>(TRoute route) where TRoute : class, IRoute;
+    }
+}
---
+++ MacroManager.Infrastructure/Abstractions/Router.cs
@@ -0,0 +1,23 @@
+﻿using MacroManager.Controllers.Navigation;
+using MacroManager.Controllers.Navigation.RouteHandlers;
+using MacroManager.Controllers.Navigation.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroManager.Infrastructure.Abstractions
+{
+    public class Router : IRouter
+    {
+
+        public void GoTo<TRoute>(TRoute route) where TRoute : class, IRoute
+        {
+            var handler = Bootstrapper.Container.GetInstance<IRouteHandler<TRoute>>();
+            handler.Handle(route);
+        }
+
+
+    }
+}
