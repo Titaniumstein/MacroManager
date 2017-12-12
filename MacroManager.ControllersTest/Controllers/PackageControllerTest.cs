ad156c707237972c4a245ee74457405e59381ae1
Titaniumstein <evan333es@gmail.com>
2017-12-12 11:46:43 -0500

    added test for package controller

---
+++ MacroManager.ControllersTest/Controllers/PackageControllerTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MacroManager.Controllers.Controllers;
+using NSubstitute;
+using MacroContext.Contract.Dto;
+using MacroContext.Contract.Commands;
+using MacroContext.Contract.Queries;
+using MacroManager.ControllersTest.TestHelpers;
+
+namespace MacroManager.ControllersTest.Controllers
+{
+    [TestClass]
+    public class PackageControllerTest
+    {
+
+        private PackageController _controller;
+        private PackageControllerMockBundle _controllerMockBundle;
+        private PackageDto _package;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _controllerMockBundle = new PackageControllerMockBundle();
+            _package = new PackageDto(Guid.NewGuid());
+            _controller = new PackageController(
+                _controllerMockBundle.MockCommandDispatcher,
+                _controllerMockBundle.MockQueryDispatcher,
+                _controllerMockBundle.MockViews
+                );
+
+        }
+
+
+        [TestMethod]
+        public void LoadIndexView_WithoutDefaultSelection()
+        {
+            _controller.LoadIndexView();
+            _controllerMockBundle.MockIndexView.Received().Initialize(null);
+        }
+
+        [TestMethod]
+        public void LoadDeleteView_CallsInitialize()
+        {
+            _controller.LoadDeleteView(_package);
+            _controllerMockBundle.MockDeleteView.Received().Initialize(_package);
+        }
+
+        [TestMethod]
+        public void LoadEditView_CallsInitialize()
+        {
+            _controller.LoadEditView(_package);
+            _controllerMockBundle.MockEditView.Received().Initialize(_package);
+        }
+
+        [TestMethod]
+        public void LoadDetailView_CallsInitialize()
+        {
+            _controller.LoadDetailView(_package);
+            _controllerMockBundle.MockDetailView.Received().Initialize(_package);
+        }
+
+
+        [TestMethod]
+        public void AddPackage_CallsCommandDispatcher()
+        {
+            _controller.AddPackage(_package);
+            _controllerMockBundle.MockCommandDispatcher.Received().Submit(Arg.Any<AddPackageCommand>());
+   
+        }
+
+        [TestMethod]
+        public void EditPackage_CallsCommandDispatcher()
+        {
+            _controller.EditPackage(_package);
+            _controllerMockBundle.MockCommandDispatcher.Received().Submit(Arg.Any<EditPackageCommand>());
+
+        }
+
+        [TestMethod]
+        public void RemovePackage_CallsCommandDispatcher()
+        {
+            _controller.RemovePackage(_package);
+            _controllerMockBundle.MockCommandDispatcher.Received().Submit(Arg.Any<RemovePackageCommand>());
+
+        }
+
+        [TestMethod]
+        public void GetAllPackages_CallsQueryDispatcher()
+        {
+            _controller.GetAllPackages();
+            _controllerMockBundle.MockQueryDispatcher.Received().Submit(Arg.Any<GetAllPackagesQuery>());
+
+        }
+
+
+    }
+}
---
+++ MacroManager.ControllersTest/TestHelpers/PackageControllerMockBundle.cs
@@ -0,0 +1,42 @@
+﻿using MacroManager.Controllers.Controllers;
+using MacroManager.Controllers.Dispatchers;
+using MacroManager.Controllers.IViews.Package;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroManager.ControllersTest.TestHelpers
+{
+    public class PackageControllerMockBundle
+    {
+        public PackageController MockController { get; private set; }
+        public ICommandDispatcher MockCommandDispatcher { get; private set; }
+        public IQueryDispatcher MockQueryDispatcher { get; private set; }
+        public ViewCollection MockViews { get; private set; }
+        public IViewIndex MockIndexView { get; private set; }
+        public IViewCreate MockCreateView { get; private set; }
+        public IViewDetail MockDetailView { get; private set; }
+        public IViewEdit MockEditView { get; private set; }
+        public IViewDelete MockDeleteView { get; private set; }
+
+
+        public PackageControllerMockBundle()
+        {
+            MockCommandDispatcher = NSubstitute.Substitute.For<ICommandDispatcher>();
+            MockQueryDispatcher = NSubstitute.Substitute.For<IQueryDispatcher>();
+            MockIndexView = NSubstitute.Substitute.For<IViewIndex>();
+            MockCreateView = NSubstitute.Substitute.For<IViewCreate>();
+            MockDetailView = NSubstitute.Substitute.For<IViewDetail>();
+            MockEditView = NSubstitute.Substitute.For<IViewEdit>();
+            MockDeleteView = NSubstitute.Substitute.For<IViewDelete>();
+            MockViews = NSubstitute.Substitute.For<ViewCollection>(MockIndexView, MockCreateView, MockEditView, MockDetailView, MockDeleteView);
+            MockController = NSubstitute.Substitute.For<PackageController>(MockCommandDispatcher, MockQueryDispatcher, MockViews);
+
+        }
+
+    }
+
+   
+}
