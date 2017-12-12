42fb724059660c50835f212d9a2e37775d976e8e
Titaniumstein <evan333es@gmail.com>
2017-12-11 22:17:38 -0500

    added a wrapper for all views per controller

---
+++ MacroManager.Controllers/IViews/IViewCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroManager.Controllers.IViews
+{
+    public interface IViewCollection<TController> 
+    {
+        void SetController(TController controller);
+       
+    }
+}
---
+++ MacroManager.Controllers/IViews/Package/ViewCollection.cs
@@ -0,0 +1,37 @@
+﻿using MacroManager.Controllers.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroManager.Controllers.IViews.Package
+{
+    public class ViewCollection : IViewCollection<PackageController>
+    {
+        public IViewIndex IndexView { get; private set; }
+        public IViewCreate CreateView { get; private set; }
+        public IViewEdit EditView { get; private set; }
+        public IViewDetail DetailView { get; private set; }
+        public IViewDelete DeleteView { get; private set; }
+
+        public ViewCollection(IViewIndex indexView, IViewCreate createView, IViewEdit editView, IViewDetail detailView, IViewDelete deleteView)
+        {
+            IndexView = indexView;
+            CreateView = createView;
+            EditView = editView;
+            DetailView = detailView;
+            DeleteView = deleteView;
+        }
+
+        public void SetController(PackageController controller)
+        {
+            IndexView.SetController(controller);
+            CreateView.SetController(controller);
+            EditView.SetController(controller);
+            DetailView.SetController(controller);
+            DeleteView.SetController(controller);
+
+        }
+    }
+}
