ab42cbfafb1ce72e54faf9448332997044a42fd4
Titaniumstein <evan333es@gmail.com>
2017-12-18 11:34:51 -0500

    added thread dispatcher for ui updates

---
+++ MacroManager.Infrastructure/Abstractions/ThreadDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using MacroManager.Controllers;
+using MacroManager.Infrastructure.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MacroManager.Infrastructure.Abstractions
+{
+    class ThreadDispatcher : IThreadDispatcher
+    {
+        public void Dispatch(Action action)
+        {
+            Application.Current.Dispatcher.Invoke(action);
+        }
+    }
+}
