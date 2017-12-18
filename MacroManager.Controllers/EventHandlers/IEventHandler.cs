67a828de5712c4a7e07a9eba15b3f02ca08d9eba
Titaniumstein <evan333es@gmail.com>
2017-12-18 12:17:57 -0500

    added eventhandler for when a package is added

---
+++ MacroManager.Controllers/EventHandlers/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroManager.Controllers.EventHandlers
+{
+    public interface IEventHandler<TEvent> //where TEvent: class
+    {
+        void Handle(TEvent e);
+
+    }
+}
---
+++ MacroManager.Controllers/EventHandlers/PackageAddedEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using MacroContext.Contract.Events;
+using MacroManager.Controllers.Controllers;
+using MacroManager.Controllers.IViews.Main;
+using MacroManager.Controllers.Navigation;
+using MacroManager.Controllers.Navigation.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace MacroManager.Controllers.EventHandlers
+{
+    public class PackageAddedEventHandler : IEventHandler<PackageAddedEvent>
+    {
+        private IViewMain _mainView;
+        private PackageController _controller;
+        private IRouter _router;
+        private IThreadDispatcher _dispatcher;
+
+        public PackageAddedEventHandler(IRouter router, IThreadDispatcher dispatcher, IViewMain mainView, PackageController controller)
+        {
+            _mainView = mainView;
+            _controller = controller;
+            _router = router;
+            _dispatcher = dispatcher;
+        }
+
+        public void Handle(PackageAddedEvent e)
+        {
+            _dispatcher.Dispatch(() => _router.GoTo(new PackageIndexRoute()));
+        }
+    }
+}
