afc7600738b449a5b35b9e71b04d376bfcb28f15
Titaniumstein <evan333es@gmail.com>
2017-12-18 12:19:43 -0500

    changed start file to program.cs for IOC WPF integration

--- MacroManager.Infrastructure/App.xaml.cs
+++ MacroManager.Infrastructure/App.xaml.cs
@@ -1,36 +1,52 @@
-﻿using MacroContext.Contract.Commands;
-using MacroContext.Contract.Dto;
-using MacroManager.Controllers.Controllers;
-using MacroManager.Infrastructure.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using MacroManager.Views.Main;
-using MacroManager.Views.Package;
-using MacroManager.Infrastructure;
-using MacroManager.Controllers.IViews.Main;
+﻿//using MacroContext.Contract.Commands;
+//using MacroContext.Contract.Dto;
+//using MacroManager.Controllers.Controllers;
+//using MacroManager.Infrastructure.Abstractions;
+//using System;
+//using System.Collections.Generic;
+//using System.Configuration;
+//using System.Data;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using MacroManager.Views.Main;
+//using MacroManager.Views.Package;
+//using MacroManager.Infrastructure;
+//using MacroManager.Controllers.IViews.Main;
+//using MacroManager.Controllers.Controllers.Orchestrator;
+//using SimpleInjector;
 
-namespace MacroManager
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        [STAThread]
-        public static void Main()
-        {
-            var application = new App();
-            application.InitializeComponent();
-            var controller = Bootstrapper.Container.GetInstance<PackageController>();
-            var view = Bootstrapper.Container.GetInstance<IViewMain>();
-            controller.LoadIndexView();
-            view.Display(controller.Views.IndexView);
-            application.Run((Window)view);
-        }
-    }
-}
+//namespace MacroManager
+//{
+//    /// <summary>
+//    /// Interaction logic for App.xaml
+//    /// </summary>
+//    public partial class App : Application
+//    {
+//        //private static Container _container;
+
+//        [STAThread]
+//        public static void Main()
+//        {
+//            using (var container = Bootstrapper.Container)
+//            {
+//                var controller = container.GetInstance<OrchestratorController>();
+//                var view = container.GetInstance<IViewMain>();
+
+//                var application = new App()
+//                {
+//                    MainWindow = (Window)view,
+//                    ShutdownMode = ShutdownMode.OnMainWindowClose
+//                };
+
+//                application.InitializeComponent();
+//                //controller.LoadStartUpView();
+//                view.Display(controller.View);
+//                application.Run(application.MainWindow);
+//            }
+            
+//        }
+
+
+//    }
+//}
---
+++ MacroManager.Infrastructure/program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SimpleInjector;
+using MacroManager.Controllers.Controllers.Orchestrator;
+using MacroManager.Controllers.IViews.Main;
+
+//this file is required for integrating wpf with simpleinjector
+//http://simpleinjector.readthedocs.io/en/latest/wpfintegration.html
+
+namespace MacroManager.Infrastructure
+{
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                using (var container = Bootstrapper.Container)
+                {
+                    RunApplication(container);
+
+                }
+
+            }
+            catch(Exception e)
+            {
+
+            }
+
+
+
+        }
+
+
+
+        private static void RunApplication(Container container)
+        {
+
+            var controller = container.GetInstance<OrchestratorController>();
+            var view = container.GetInstance<IViewMain>();
+
+            var application = new App()
+            {
+                MainWindow = (Window)view,
+                ShutdownMode = ShutdownMode.OnMainWindowClose
+            };
+
+            //application.InitializeComponent();
+            view.Display(controller.View);
+            controller.Initialize();
+            application.Run(application.MainWindow);
+
+
+            
+
+        }
+
+    }
+}
+
